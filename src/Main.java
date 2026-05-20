ce16dbee99f4af23e90f87f790a0b8553ab6ae43
Nenerox <andres.ps0308@gmail.com>
2026-05-20 17:00:54 -0600

    Se completo el insertar pacientes en el heap y poder ver el paciente actual hasta llegar al ultimo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TxtReader reader = new TxtReader();
+        VectorHeap<Paciente> minheap = new VectorHeap<>();
+
+        String[] pacientes = reader.readFile("src\\pacientes.txt", "\n");
+
+        for (String paciente : pacientes) {
+            String[] info = paciente.split(",");
+            String nombre = info[0];
+            String sintoma = info[1];
+            String prioridad = info[2];
+            Paciente p = new Paciente(nombre, sintoma, prioridad);
+            minheap.insert(p);
+        }
+        
+        while (!minheap.isEmpty()) {
+            System.out.println("Paciente actual: " + minheap.remove());
+            System.out.println("Presione enter para el siguiente paciente");
+            scanner.nextLine();
+        }
+        System.out.println("No hay más pacientes en espera.");
     }
 }
